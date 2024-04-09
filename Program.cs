07661607f4bb1280e24b39f09bc0215d3c1608e2
charlesperrillat <ce.perrillat@gmail.com>
2024-04-09 14:43:13 +0200

    amended the program to handle every situation

--- Program.cs
+++ Program.cs
@@ -1,25 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Enter a minimum number:");
+Console.Write("Enter a minimum number: ");
 int limit1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter a maximum number:");
+Console.Write("Enter a maximum number: ");
 int limit2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter an in-between number:");
+Console.Write("Enter an in-between number: ");
 int integer = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Limit 1: " + limit1);
 Console.WriteLine("Limit 2: " + limit2);
 Console.WriteLine("Integer to limit: " + integer);
 
-if (integer < limit1)
+if (limit1 < limit2 && integer < limit1)
 {
     Console.WriteLine("You have entered " + integer + " which is lower than " + limit1 + " which is the minimum");
     Console.WriteLine("Resulting integer: " + limit1);
 }
-else if (integer > limit2)
+else if (limit1 < limit2 && integer > limit2)
 {
     Console.WriteLine("You have entered " + integer + " which is greater than " + limit2 + " which is the maximum");
     Console.WriteLine("Resulting integer: " + limit2);
+}
+else if (limit1 > limit2 && integer < limit2)
+{
+    Console.WriteLine("You have entered " + integer + " which is lower than " + limit2 + " which is the minimum");
+    Console.WriteLine("Resulting integer: " + limit2);
+}
+else if (limit1 > limit2 && integer > limit1)
+{
+    Console.WriteLine("You have entered " + integer + " which is greater than " + limit1 + " which is the maximum");
+    Console.WriteLine("Resulting integer: " + limit1);
 }
 else
     Console.WriteLine("Good job!");
